277c598907b5753254211022ab891a370d15d9b1
b96705008 <b96705008@gmail.com>
2017-12-02 14:28:42 +0800

    preprocessing

--- start-notebook.sh
+++ start-notebook.sh
@@ -4,4 +4,11 @@ export PYSPARK_DRIVER_PYTHON_OPTS="notebook --port 9527"
 pyspark \
         --name KKTV_Spark \
         --master local[*] \
-        --executor-memory 4G
+        --driver-cores 1 \
+        --driver-memory 1g \
+        --executor-memory 2G \
+        --conf spark.shuffle.service.enabled=true \
+        --conf spark.dynamicAllocation.enabled=true \
+        --conf spark.dynamicAllocation.maxExecutors=6 \
+        --conf spark.dynamicAllocation.executorIdleTimeout=1s \
+        --conf spark.dynamicAllocation.minExecutors=1
